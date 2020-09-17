7f67a4ac1c335c79c28a451de2228e1a88c0ef5a
zjschulz <zjschulz@gmail.com>
2020-09-17 15:31:59 +0000

    update character migration file with attributes

--- dndmaster-backend/db/migrate/20200917142024_create_characters.rb
+++ dndmaster-backend/db/migrate/20200917142024_create_characters.rb
@@ -2,7 +2,15 @@ class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
       t.string :name
-
+      t.string :race
+      t.string :character_class
+      t.integer :background
+      t.integer: level
+      t.integer :armor_class
+      t.integer :perception
+      t.integer :investigation
+      t.integer :insight
+      t.integer :spell_save_dc
       t.timestamps
     end
   end
