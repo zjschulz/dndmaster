72d17785bf19bebe962ec4d7cd483ebb247b777e
zjschulz <zjschulz@gmail.com>
2020-09-17 14:51:34 +0000

    correct migration table for relationships

--- dndmaster-backend/db/migrate/20200917141952_create_campaigns.rb
+++ dndmaster-backend/db/migrate/20200917141952_create_campaigns.rb
@@ -2,6 +2,7 @@ class CreateCampaigns < ActiveRecord::Migration[6.0]
   def change
     create_table :campaigns do |t|
       t.integer :user_id
+      t.integer :character_id
       t.string :name
 
       t.timestamps
--- dndmaster-backend/db/migrate/20200917142024_create_characters.rb
+++ dndmaster-backend/db/migrate/20200917142024_create_characters.rb
@@ -1,10 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
-      t.integer :user_id
-      t.integer :campaign_id
       t.string :name
-      
+
       t.timestamps
     end
   end
