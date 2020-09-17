0a175b02c5f6fa73d1e1a22b70c397d87f43cfa4
zjschulz <zjschulz@gmail.com>
2020-09-17 14:49:16 +0000

    create campaign and characters models

--- dndmaster-backend/app/models/user.rb
+++ dndmaster-backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates_presence_of :email
     validates_uniqueness_of :email
+    has_many :campaigns
+    has_many :characters, through: :campaigns
 end
---
+++ dndmaster-backend/db/migrate/20200917141952_create_campaigns.rb
@@ -0,0 +1,10 @@
+class CreateCampaigns < ActiveRecord::Migration[6.0]
+  def change
+    create_table :campaigns do |t|
+      t.integer :user_id
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
