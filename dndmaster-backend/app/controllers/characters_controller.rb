02e28de7d4ed41ae02c52b3509bf3d20af86828a
zjschulz <zjschulz@gmail.com>
2020-09-21 13:45:15 +0000

    create character create action

--- dndmaster-backend/app/controllers/characters_controller.rb
+++ dndmaster-backend/app/controllers/characters_controller.rb
@@ -10,6 +10,19 @@ class CharactersController < ApplicationController
     end
 
     def create
+        user = User.find_by(id: params[:user_id])
+        campaign = Campaign.find_by(id: params[:campaign_id])
+        character = Character.create(name: params[:name], user: user, campaign: campaign)
+
+        if character.id
+            render json: {
+                status: :created
+            }
+        else
+            render json: {
+                status: :error
+            }
+        end
     end
 
     def update
